f3f3e6a1951613b08efe19110cb7c991d6c7dc61
LamDoon <71416214+LamDoon@users.noreply.github.com>
2020-10-25 15:37:35 +0700

    Add files via upload

---
+++ frofile_user.java
@@ -0,0 +1,16 @@
+package com.example.profileuser;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+public class frofile_user extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+}
